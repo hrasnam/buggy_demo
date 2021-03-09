78daf10fd95b41006f25c9d139f8d08d356bcd36
hrasnam <marekhrasna@outlook.com>
2021-03-09 10:34:52 +0100

    fixed a bug

--- fail.cpp
+++ fail.cpp
@@ -12,13 +12,16 @@ int memcheckFailDemo(int* arrayStack, unsigned int arrayStackLen,
   int* array = new int[5];
   array[100] = 0; 
 
-  arrayStack[100] = 0; 
-  arrayHeap[100] = 0; 
-
-  for (unsigned int i = 0; i <= arrayStackLen; i++) {
+  if (arrayStackLen > 100) {
+      arrayStack[100] = 0;
+  }
+  if (arrayHeapLen > 100) {
+      arrayHeap[100] = 0;
+  }
+  for (unsigned int i = 0; i < arrayStackLen; i++) {
       arrayStack[i] = 0;
   }
-  for (unsigned int i = 0; i <= arrayHeapLen; i++) {
+  for (unsigned int i = 0; i < arrayHeapLen; i++) {
       arrayHeap[i] = 0;
   }
 
